f66caed4c810df25ee8f7c9bef555cd77901beb7
erlendsyntra <erlendgeerts@gmail.com>
2023-12-11 17:43:04 +0100

    with flip cards

--- src/components/RandomUsers/RandomUsers.jsx
+++ src/components/RandomUsers/RandomUsers.jsx
@@ -15,16 +15,42 @@ const RandomUsers = () => {
     getData();
   }, []);
 
+  // return (
+  //   <div id="randomUsersContainer" className="wrapper">
+  //     {data && (
+  //       <>
+  //         {data.map((el, i) => (
+  //           <div className="person" key={i}>
+  //             <img src={el.picture.large} alt="" />
+  //             <p>
+  //               {el.name.title}. {el.name.first} {el.name.last}
+  //             </p>
+  //           </div>
+  //         ))}
+  //       </>
+  //     )}
+  //   </div>
+  // );
   return (
     <div id="randomUsersContainer" className="wrapper">
       {data && (
         <>
           {data.map((el, i) => (
-            <div className="person" key={i}>
-              <img src={el.picture.large} alt="" />
-              <p>
-                {el.name.title}. {el.name.first} {el.name.last}
-              </p>
+            <div className="flip-card">
+              <div className="flip-card-inner">
+                <div className="flip-card-front">
+                  <img
+                    src={el.picture.large}
+                    alt="Avatar"
+                    style={{ width: "128px", height: "128px" }}
+                  />
+                </div>
+                <div className="flip-card-back">
+                  <p>
+                    {el.name.title}. {el.name.first} {el.name.last}
+                  </p>
+                </div>
+              </div>
             </div>
           ))}
         </>
